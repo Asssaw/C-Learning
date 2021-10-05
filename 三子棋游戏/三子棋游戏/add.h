eca331ed5379868a87041596493e4d3dcab7776a
Asssaw <91747799+Asssaw@users.noreply.github.com>
2021-10-05 08:22:26 +0800

    三子棋升级版
    电脑更加智能：会首先判断能否获胜，如果不能就判断是否需要堵棋，如果都不需要才随机落子

--- 三子棋游戏/三子棋游戏/add.h
+++ 三子棋游戏/三子棋游戏/add.h
@@ -1,7 +1,16 @@
+#include<stdio.h>
+#include<time.h>
+#define COL 3
+#define ROW 3
+
+
 #ifndef __ADD_H__
 #define __ADD_H__
 
 void menu();
-
+void player(char board[][COL]);
+void computer(char board[][COL]);
+void show(char board[][COL]);
+void exam(char board[][COL], int* a);
 #endif
 
